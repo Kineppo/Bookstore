49c60391133a866ce00a0a7dc89ec9d9db54aba6
Moisanen Tiina <a1503046@myy.haaga-helia.fi>
2017-02-16 17:14:59 +0200

    Kaunisteltu booklistia

---
+++ src/main/java/com/tepsun/Controller/UserDetailServiceImpl.java
@@ -0,0 +1,11 @@
+/*package com.tepsun.Controller;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+import com.tepsun.model.UserRepository;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService{
+	private final UserRepository repository;
+}*/
