8017c46eee4c93ae32557937344b8483d1c8ea4d
Kine <Kine>
2017-03-02 01:16:52 +0200

    Restin lisäys

--- src/main/java/com/tepsun/Controller/BookController.java
+++ src/main/java/com/tepsun/Controller/BookController.java
@@ -1,11 +1,14 @@
 package com.tepsun.Controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tepsun.model.Book;
 import com.tepsun.model.BookRepository;
@@ -23,6 +26,17 @@ public class BookController {
 		model.addAttribute("books", bookRepository.findAll());
 		return "booklist";
 	}
+	
+	@RequestMapping(value="/books", method = RequestMethod.GET)
+	public @ResponseBody List<Book> studentListRest(){
+		return(List<Book>)bookRepository.findAll();
+	}
+	
+	@RequestMapping(value="/book/{id}", method = RequestMethod.GET)
+	public @ResponseBody Book findBookRest(@PathVariable("id")Long bookId){
+		return bookRepository.findOne(bookId);
+	}
+		
 	
 	@RequestMapping(value = "/add")
     public String addBook(Model model){
